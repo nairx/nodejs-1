f76ee6dd23437c345cfab09eb3c813b18509efc8
nairx <nairxprv@gmail.com>
2026-03-02 19:34:46 +0530

    updated

---
+++ index18a.js
@@ -0,0 +1,6 @@
+//Accept two numbers through command line argument
+//and print the sum and upload to github, put the message
+
+let num1 = process.argv[2]
+let num2 = process.argv[3]
+console.log(Number(num1) + Number(num2))
